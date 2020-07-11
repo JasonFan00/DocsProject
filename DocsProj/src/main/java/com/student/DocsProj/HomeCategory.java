675d91b34a7d519640312434e9122346458cc2af
Jason <47374783+JasonFang00@users.noreply.github.com>
2020-07-11 15:49:27 -0400

    home page

---
+++ DocsProj/src/main/java/com/student/DocsProj/HomeCategory.java
@@ -0,0 +1,11 @@
+package com.student.DocsProj;
+
+/**
+ * Each HomeCategory object represents a category on the home page
+ * @author Jason Fan
+ *
+ */
+
+public class HomeCategory {
+
+}
---
+++ DocsProj/src/main/java/com/student/DocsProj/HomeStructure.java
@@ -0,0 +1,14 @@
+package com.student.DocsProj;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Class to hold the list representing the structure of the home page
+ * @author Jason Fan
+ *
+ */
+
+@Component
+public class HomeStructure {
+	
+}
