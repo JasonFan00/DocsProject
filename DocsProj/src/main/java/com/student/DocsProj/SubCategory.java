6b6e4b0b43ee87771ce2c526c01a0959c873d243
Staci Weeks <stacimweeks@gmail.com>
2020-07-11 15:10:33 -0500

    Create SubCategory.java

---
+++ DocsProj/src/main/java/com/student/DocsProj/SubCategory.java
@@ -0,0 +1,20 @@
+package com.student.DocsProj;
+
+public class SubCategory {
+	
+	public String subCategoryName;
+	public String url;
+	public double num;
+	
+	public String getsubCategoryName() {
+		return this.subCategoryName;
+	}
+	
+	public String getSubCategoryUrl() {
+		return this.url;
+	}
+	
+	public double getSubCategoryNum() {
+		return this.num;
+	}
+}
