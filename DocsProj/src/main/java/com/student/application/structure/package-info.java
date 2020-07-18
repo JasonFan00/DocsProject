3211e80cd453fc9aecf1f85dd910b9cec5d5da2c
Jason <47374783+JasonFang00@users.noreply.github.com>
2020-07-18 18:33:13 -0400

    comments

--- DocsProj/src/main/java/com/student/application/structure/package-info.java
+++ DocsProj/src/main/java/com/student/application/structure/package-info.java
@@ -1,5 +1,5 @@
 /**
- * Represents the site structure of the generated pages (url to page, page title, etc.), which will then be used in generating the home page with thyme leaf
+ * Represents the site structure of the generated pages (url to page, page title, etc.), which will then be used in generating the home page with thyme leaf.  Will also support nested categories.
  */
 /**
  * @author Jason Fan
